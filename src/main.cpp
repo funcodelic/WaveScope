6e06d0c11a6037fccf9a6232911c9f7075e7b3ae
Troy Mulder <126816710+funcodelic@users.noreply.github.com>
2026-02-01 14:12:08 -0500

    Restructure project into src/include/build layout

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+*
+*
+*
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "running WaveScope...\n";
+    cout << "_cplusplus = " << __cplusplus << endl;
+
+    return 0;
+}
+
