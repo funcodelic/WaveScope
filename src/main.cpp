9d63f0888eb8514fb3e358f6aab52c93dffc245b
Troy Mulder <126816710+funcodelic@users.noreply.github.com>
2026-02-02 13:24:19 -0500

    Add Qt 6.10.1 CMake setup and basic Qt window

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,23 @@
 *
 *
 */
+#include <QApplication>
+#include <QWidget>
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
     cout << "running WaveScope...\n";
     cout << "_cplusplus = " << __cplusplus << endl;
+
+
+    QApplication app(argc, argv);
+    QWidget w;
+    w.setWindowTitle("WaveScope");
+    w.resize(400, 300);
+    w.setStyleSheet("background-color: #2b6cff;");
+    w.show();
+    return app.exec();
 
     return 0;
 }
